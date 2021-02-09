096e3aecb8f025c9ae8ad7e232fc21c238dacee2
Ed McCormic <ed@edmccormic.com>
2021-02-08 20:00:57 -0500

    change

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // Import dependencies
-import React, { useRef, useState, useEffect } from "react";
+import React, { useRef, useEffect } from "react";
 import * as tf from "@tensorflow/tfjs";
 import * as cocossd from "@tensorflow-models/coco-ssd";
 import Webcam from "react-webcam";
